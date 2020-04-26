6ddf8a2c0c5efdc9e39c3ebe0ec64ae9839bccea
Cameron Pickle <cmpickle@gmail.com>
2020-04-26 16:47:37 -0600

    Add button lessons

---
+++ Debounce_Button/Debounce_Button.ino
@@ -0,0 +1,54 @@
+// constants won't change. They're used here to set pin numbers:
+const int buttonPin = 2;     // the number of the pushbutton pin
+const int redLedPin =  13;      // the number of the red LED pin
+
+// variables will change:
+int buttonState;         // variable for reading the pushbutton status
+int lastButtonState = LOW;         // variable for reading the pushbutton status
+int lightState = HIGH;
+
+// the following variables are unsigned longs because the time, measured in
+// milliseconds, will quickly become a bigger number than can be stored in an int.
+unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
+unsigned long debounceDelay = 50;    // the debounce time; increase if the output flickers
+
+void setup() {
+  // initialize the LED pins as an outputs:
+  pinMode(redLedPin, OUTPUT);
+  // initialize the pushbutton pin as an input:
+  pinMode(buttonPin, INPUT);
+}
+
+void loop() {
+  // read the state of the pushbutton value:
+  int reading = digitalRead(buttonPin);
+
+  // check to see if you just pressed the button
+  // (i.e. the input went from LOW to HIGH), and you've waited long enough
+  // since the last press to ignore any noise:
+
+  // If the switch changed, due to noise or pressing:
+  if (reading != lastButtonState) {
+    // reset the debouncing timer
+    lastDebounceTime = millis();
+  }
+
+
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    // whatever the reading is at, it's been there for longer than the debounce
+    // delay, so take it as the actual current state:
+
+    // if the button state has changed:
+    if (reading != buttonState) {
+      buttonState = reading;
+      if (reading == HIGH) {
+        lightState = !lightState;
+      }
+    }
+  }
+
+  digitalWrite(redLedPin, lightState);
+
+  // save the reading. Next time through the loop, it'll be the lastButtonState:
+  lastButtonState = reading;
+}
