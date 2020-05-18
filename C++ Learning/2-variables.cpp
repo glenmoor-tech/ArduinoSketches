2b543aead61d8c587db2cef65602c41bb04ab4cf
Cameron Pickle <cmpickle@gmail.com>
2020-05-17 22:42:43 -0600

    Update lessons

--- C++ Learning/2-variables.cpp
+++ C++ Learning/2-variables.cpp
@@ -5,13 +5,14 @@ int main() {
     int number = 1; // A whole number in the range -2,147,483,648 to 2,147,483,647
     char letter = 'a'; // Any single character value
     float decimal = 1.5; // decimal numbers
-
-    char* sentance = "Hello World"; // strings are many characters next to each other.
+    bool trueOrFalse = true; // A value of true or false
+    string sentance = "Hello World"; // strings are many characters next to each other.
                                     // They can be thought of as sentances.
 
     // Use the << operator to switch between writing strings and outputting a variable.
     cout << "Here is my number: " << number << "\n";
     cout << "Here is my letter: " << letter << "\n";
     cout << "Here is my decimal: " << decimal << "\n";
+    cout << "Here is my boolean: " << trueOrFalse << "\n";
     cout << "Here is my sentance: " << sentance << "\n";
 }
